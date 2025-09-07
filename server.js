ea4f6343d797f2f7427099849d0e454c0369c863
meztinosdunken <orlanmorry5462@hotmail.com>
2025-09-07 14:29:23 -0500

    Add basic error handling middleware

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Agile Coaching Tool!');
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
